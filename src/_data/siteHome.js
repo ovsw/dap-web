6edf8212ab80ec826275a61ecabdfa20cc625aa1
Ovi Savescu <ovi@ovswebsites.com>
2021-01-04 12:21:50 +0200

    changed query to follow references for reused sections

--- src/_data/siteHome.js
+++ src/_data/siteHome.js
@@ -3,7 +3,20 @@ const client = require('../utils/sanityClient')
 module.exports =  async function() {
   const sanityResponse = await client.fetch(groq`
     *[_id == "siteHome"]{
-      ...
+      ...,
+      content {
+        hero,
+        sections[]{
+          ...,
+          _type == 'reusedSection' => {
+            ...,
+            reusableSection->{
+              ...
+            }
+          }
+        }
+      }
+      
     }[0]
   `).catch(err => console.error(err))
 
